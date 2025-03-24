5745547aed4230c4085305827db3a38f3e0b161f
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-03-24 17:51:07 +0000

    Completed Part A

--- MyFirstGUI.py
+++ MyFirstGUI.py
@@ -11,13 +11,26 @@ class MyFirstGUI:
         self.entry1 = Entry()
         self.entry1.pack()
 
-        self.label2 = Label(master, text="Enter your Surname")
+        self.label2 = Label(master, text="Enter your Date of Birth")
         self.label2.pack()
         self.entry2 = Entry()
         self.entry2.pack()
+
+        self.label3 = Label(master, text="Enter your Firstname")
+        self.label3.pack()
+        self.entry3 = Entry()
+        self.entry3.pack()
+
+        self.label4 = Label(master, text="Enter your Member Type")
+        self.label4.pack()
+        self.entry4 = Entry()
+        self.entry4.pack()
         
-        self.helloButton = Button(master, text="Hello", command=self.sayhello)
-        self.helloButton.pack()
+        self.insertintodbButton = Button(master, text="Insert Into DB", command=self.sayhello)
+        self.insertintodbButton.pack()
+
+        self.printallmembersButton = Button(master, text="Print All Members", command=self.sayhello)
+        self.printallmembersButton.pack()
 
         self.closeButton = Button(master, text="Close", command=self.close)
         self.closeButton.pack()
